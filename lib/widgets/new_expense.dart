ae687c80c0fe54a9f712d3fb667dcec9755c9bbd
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2023-09-05 17:21:16 +0600

    added modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -6,28 +6,65 @@ class NewExpense extends StatefulWidget {
   @override
   State<NewExpense> createState() => _NewExpenseState();
 }
-  class _NewExpenseState extends State<NewExpense>{
-    @override
-    Widget build(BuildContext context){
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "Expense Name"),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Expense Amount"),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Expense Date"),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Expense Category"),
-            ),
-            ElevatedButton(onPressed: (){}, child: Text("Add Expense"))
-          ],
-        ),
-      );
-    }
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            decoration: InputDecoration(labelText: "Expense Name"),
+            maxLength: 50,
+          ),
+          Row(children: [
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                decoration:
+                    InputDecoration(labelText: "Amount", prefixText: "\$"),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            const SizedBox(width: 18),
+            Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+              Text("Selected Date"),
+              IconButton(onPressed: () {}, icon: Icon(Icons.calendar_today)),
+            ]))
+          ]),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Cancel"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text("Add Expense"))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
